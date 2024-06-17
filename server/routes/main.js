55bce87ff4eb339aae2abec6cd5d8d5df0f72730
rajat jana <rajatjana2024@gmail.com>
2024-06-17 15:20:57 +0530

    paginatioin done

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,19 +3,45 @@ const router = express.Router();
 const Post = require('../models/Post');
 
 router.get('/',async (req, res) => {
-    const locals = {
-        title: 'Home',
-        description: 'Welcome to the home page'
-    };
+   
 
     try{
-        const data = await Post.find().sort({createdAt: 'desc'});
-        res.render('index',{locals,data});
+        const locals = {
+            title: 'Home',
+            description: 'Welcome to the home page'
+        };
+
+        let perPage = 2;
+        let page = parseInt(req.query.page) || 1;
+        
+
+
+        console.log("page: ",page);
+        const data = await  Post.aggregate([{ $sort: { createdAt: -1 } }])
+        .skip(perPage * (page - 1))
+        .limit(perPage)
+        .exec();
+
+        console.log(data);
+
+        const count = await Post.countDocuments();
+        const nextPage = parseInt(page) + 1;
+        const hashNextPage = nextPage <= Math.ceil(count/perPage);
+
+       
+        res.render('index', {
+            locals,
+            data,
+            current: page,
+            nextPage: hashNextPage ? nextPage : null,
+            currentRoute: '/',
+        });
+
+        console.log("clicked..");
+        
     }catch(error){
         console.log(error.message);
     }
-
-
 
 });
 
@@ -24,6 +50,23 @@ router.get('/',async (req, res) => {
 router.get('/about', (req, res) => {
     res.render('about');
 });
+
+
+
+// router.get('/',async (req, res) => {
+//     const locals = {
+//         title: 'Home',
+//         description: 'Welcome to the home page'
+//     };
+
+//     try{
+//         const data = await Post.find().sort({createdAt: 'desc'});
+//         res.render('index',{locals,data});
+//     }catch(error){
+//         console.log(error.message);
+//     }
+
+// });
 
 
 module.exports = router;
