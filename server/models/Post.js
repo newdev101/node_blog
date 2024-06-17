2e734092c597cdfd545872f1e87c3acb5b7a54e8
rajat jana <rajatjana2024@gmail.com>
2024-06-17 14:20:35 +0530

    showing blog posts in home page

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,6 +1,6 @@
-const mongoose = 'mongoose';
-
-const PostSchema = new mongoose.Schema({
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const PostSchema = new Schema({
      title:{
           type: String,
           required: true
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
+router.get('/',async (req, res) => {
     const locals = {
         title: 'Home',
         description: 'Welcome to the home page'
     };
-    res.render('index',{locals});
+
+    try{
+        const data = await Post.find().sort({createdAt: 'desc'});
+        res.render('index',{locals,data});
+    }catch(error){
+        console.log(error.message);
+    }
+
+
+
 });
+
+
 
 router.get('/about', (req, res) => {
     res.render('about');
